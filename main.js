bf1007583bbb58968970a07d344bb53cc36b22c8
dircielesoares <105812518+dircielesoares@users.noreply.github.com>
2023-10-19 16:51:19 -0300

    Add files via upload
    19/10 finalizado

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom (selectorAudio) {
+   const elemento = document.querySelector(selectorAudio);
+
+   if (elemento && elemento.localName === 'audio') {
+          elemento.play();
+       }   
+       else {
+       //alert('Elemento não encontrado')
+      console.log('Elemento não encontrado ou seletor inválido');
+       }
+
+   }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-//enquanto
-while (contador = listaDeTeclas.length) {
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumento}`;
-
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
+    
+    tecla.onkeydown = function (evento) {
 
-    contador = contador + 1;
-
-    //console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+     }
+    
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
+
+ 
